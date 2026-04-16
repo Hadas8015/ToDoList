9abf115027b482c2e2b727a3360ca63e80a55a93
Hadas8015 <h0504138015@gmail.com>
2026-04-17 02:54:19 +0300

    updated api url

--- frontend/src/service.js
+++ frontend/src/service.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 //הגדרת כתובת ברירת מחדל 
-axios.defaults.baseURL = "http://localhost:5238";
+axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
 
 //לשגיאות בתשובות מהשרת
 axios.interceptors.response.use(
